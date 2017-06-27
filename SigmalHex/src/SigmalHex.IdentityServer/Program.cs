c731c286b9b23a6c0244c041237da7453dee9dab
zhenpeng <zhenpeng@ctrip.com>
2017-06-27 16:41:47 +0800

    https://github.com/pzdn2009/TechStack/projects/2#card-3524060

---
+++ SigmalHex/src/SigmalHex.IdentityServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
+
+namespace SigmalHex.IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
